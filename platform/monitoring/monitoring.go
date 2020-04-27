88ca71429ad27f190032bc310eaece8607557ee2
Kemosabert <bert.coppens14@gmail.com>
2020-04-27 12:34:45 +0200

    fmt code

--- platform/monitoring/monitoring.go
+++ platform/monitoring/monitoring.go
@@ -5,21 +5,21 @@ import (
 )
 
 type Config struct {
-	AttachStacktrace bool `json:"attach_stack_trace"`
-	DSN string  `json:"dsn"`
-	ReleaseName   string  `json:"release_name"`
-	Environment   string  `json:"environment"`
-	Debug   bool `json:"debug"`
-	SampleRate float64 `json:"sample_rate"`
+	AttachStacktrace bool    `json:"attach_stack_trace"`
+	DSN              string  `json:"dsn"`
+	ReleaseName      string  `json:"release_name"`
+	Environment      string  `json:"environment"`
+	Debug            bool    `json:"debug"`
+	SampleRate       float64 `json:"sample_rate"`
 }
 
 var DefaultConfig = Config{
 	AttachStacktrace: true,
-	DSN: "",
-	ReleaseName:   "",
-	Environment:   "development",
-	Debug:  false,
-	SampleRate: 0.8,
+	DSN:              "",
+	ReleaseName:      "",
+	Environment:      "development",
+	Debug:            false,
+	SampleRate:       0.8,
 }
 
 func CreateNewErrorMonitoring(cfg Config) error {
