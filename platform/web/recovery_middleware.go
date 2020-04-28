0a6e476f66c1b2bb39ae90ffe7edb04597d422d0
Kemosabert <bert.coppens14@gmail.com>
2020-04-28 09:56:32 +0200

    :sparkles: Add custom middleware to catch and handle panics

---
+++ platform/web/recovery_middleware.go
@@ -0,0 +1,41 @@
+package web
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"go.opencensus.io/trace"
+)
+
+func RecoveryMiddleware() Middleware {
+	return func(before Handler) Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+			ctx, span := trace.StartSpan(ctx, "internal.web.RecoveryMiddleware")
+
+			defer func() {
+				err := recover()
+				if err != nil {
+					v := ctx.Value(KeyValues).(*Values)
+
+					sentry.WithScope(func(scope *sentry.Scope) {
+						scope.SetTag("Trace-ID",  v.TraceID)
+
+						sentry.CurrentHub().Recover(err)
+					})
+					sentry.Flush(2 * time.Second)
+
+					_ = Respond(ctx, w, NewErrUnmanagedResponse(v.TraceID), http.StatusInternalServerError)
+				}
+				span.End()
+			}()
+
+			_ = before(ctx, w, r, params)
+
+			return nil
+		}
+
+		return h
+	}
+}
