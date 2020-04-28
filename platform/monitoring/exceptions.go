406f1567317f6ce04df5efdf2104c3db13d61a19
Kemosabert <bert.coppens14@gmail.com>
2020-04-28 09:56:04 +0200

    :recycle: Enhance the exceptions

--- platform/monitoring/exceptions.go
+++ platform/monitoring/exceptions.go
@@ -4,15 +4,17 @@ import (
 	"github.com/getsentry/sentry-go"
 )
 
-type LogLevel struct {
-	Debug, Info, Warning, Error, Fatal sentry.Level
-}
 
 type Exception struct {
-	Level    *sentry.Level
-	Tags     map[string]string
-	Contexts map[string]string
-	Err      error
+	Level      *sentry.Level
+	Tags       map[string]string
+	Contexts   map[string]interface{}
+	Additional map[string]string
+	Err        error
+}
+
+type LogLevel struct {
+	Debug, Info, Warning, Error, Fatal sentry.Level
 }
 
 var LogLevels = LogLevel{
@@ -25,9 +27,10 @@ var LogLevels = LogLevel{
 
 func CaptureException(exception error) Exception {
 	return Exception{
-		Err: exception,
-		Tags: make(map[string]string, 0),
-		Contexts: make(map[string]string, 0),
+		Err:        exception,
+		Tags:       make(map[string]string, 0),
+		Contexts:   make(map[string]interface{}, 0),
+		Additional: make(map[string]string, 0),
 	}
 }
 
@@ -43,20 +46,30 @@ func (exception Exception) AddTag(key string, value string) Exception {
 	return exception
 }
 
-func (exception Exception) AddContext(key string, context string) Exception {
+func (exception Exception) AddContext(key string, context interface{}) Exception {
 	exception.Contexts[key] = context
+
+	return exception
+}
+
+func (exception Exception) AddAdditional(key string, data string) Exception {
+	exception.Additional[key] = data
 
 	return exception
 }
 
 func (exception Exception) Log() {
 	sentry.WithScope(func(scope *sentry.Scope) {
-		for tagName, tag := range exception.Tags {
-			scope.SetTag(tagName, tag)
+		for key, tag := range exception.Tags {
+			scope.SetTag(key, tag)
 		}
 
-		for contextName, context := range exception.Contexts {
-			scope.SetContext(contextName, context)
+		for key, context := range exception.Contexts {
+			scope.SetContext(key, context)
+		}
+
+		for key, data := range exception.Additional {
+			scope.SetExtra(key, data)
 		}
 
 		if exception.Level != nil {
