91be33d03dc6eda92aabaa3da93d57b42186138e
Kemosabert <bert.coppens14@gmail.com>
2020-05-13 14:45:31 +0200

    Add a health check endpoint for the database (#12)
    * :sparkles: Add a health check handler for the database connection
    
    * :sparkles: Add the database health check as a default health checker
    
    * Add error message to span
    
    * format file

---
+++ platform/database/health.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"go.opencensus.io/trace"
+
+	"github.com/fewlinesco/go-pkg/platform/web"
+)
+
+func HealthCheck(db *sqlx.DB) web.HealthzChecker {
+	return func(ctx context.Context) web.HealthzStatus {
+		ctx, span := trace.StartSpan(ctx, "database.HealthChecker")
+		span.End()
+
+		service := web.HealthzStatus{
+			Type:        "Database",
+			Description: "Check the availability of the service's database",
+			State:       web.HealthzStateHealthy,
+		}
+
+		err := db.PingContext(ctx)
+		if err != nil {
+			errorMessage := err.Error()
+
+			service.Error = errorMessage
+			span.AddAttributes(trace.StringAttribute("database-health-error", errorMessage))
+
+			service.State = web.HealthzStateUnhealthy
+		}
+
+		return service
+	}
+}
