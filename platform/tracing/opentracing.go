f750aebed1025edb0b55068bb5dc47b2c3c26737
Kemosabert <bert.coppens14@gmail.com>
2020-05-29 12:59:55 +0200

    Hide values from tracing attributes (#16)
    * add function to add disclosed attributes to tracing spans
    
    * Add function to add disclosed attributes to a span
    
    * implement feedback

--- platform/tracing/opentracing.go
+++ platform/tracing/opentracing.go
@@ -42,3 +42,23 @@ func Start(cfg Config) error {
 
 	return nil
 }
+
+// AddAttribute can add a string attribute to the provided span
+// Every value which is not an empty string will be changed to "[REDACTED]"
+// This masks the actual value but indicates a certain key does have a value
+// Empty values will be represented by ""
+func AddAttribute(span *trace.Span, key string, value string) {
+	if len(value) != 0 {
+		value = "[REDACTED]"
+	}
+
+	attribute := trace.StringAttribute(key, value)
+
+	span.AddAttributes(attribute)
+}
+
+// AddAttributeWithDisclosedData adds a string attribute to a span without concealing it's value
+func AddAttributeWithDisclosedData(span *trace.Span, key string, value string) {
+	attribute := trace.StringAttribute(key, value)
+	span.AddAttributes(attribute)
+}
