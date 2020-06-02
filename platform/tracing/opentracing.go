2e7d638c08c239e0afa7cfc34865297d511e7231
Kemosabert <bert.coppens14@gmail.com>
2020-06-02 11:34:15 +0200

    :sparkles: Add an abstraction to the go-pkg to create spans

--- platform/tracing/opentracing.go
+++ platform/tracing/opentracing.go
@@ -1,9 +1,14 @@
 package tracing
 
 import (
+	"context"
+
 	"contrib.go.opencensus.io/exporter/jaeger"
 	"go.opencensus.io/trace"
 )
+
+// Span represents an individual unit of work in the system
+type Span = trace.Span
 
 // Config represents the JSON config applications can define in order to configure tracing
 type Config struct {
@@ -47,7 +52,7 @@ func Start(cfg Config) error {
 // Every value which is not an empty string will be changed to "[REDACTED]"
 // This masks the actual value but indicates a certain key does have a value
 // Empty values will be represented by ""
-func AddAttribute(span *trace.Span, key string, value string) {
+func AddAttribute(span *Span, key string, value string) {
 	if len(value) != 0 {
 		value = "[REDACTED]"
 	}
@@ -58,7 +63,12 @@ func AddAttribute(span *trace.Span, key string, value string) {
 }
 
 // AddAttributeWithDisclosedData adds a string attribute to a span without concealing it's value
-func AddAttributeWithDisclosedData(span *trace.Span, key string, value string) {
+func AddAttributeWithDisclosedData(span *Span, key string, value string) {
 	attribute := trace.StringAttribute(key, value)
 	span.AddAttributes(attribute)
+}
+
+// StartSpan creates a new span with the provided name
+func StartSpan(ctx context.Context, name string) (context.Context, *Span) {
+	return trace.StartSpan(ctx, name)
 }
