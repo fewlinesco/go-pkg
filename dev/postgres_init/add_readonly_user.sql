e437f27783d8aaeff96d7fd59eaaee301ba5edb6
Kemosabert <bert.coppens14@gmail.com>
2020-09-09 15:05:52 +0200

    change comment so it's more specific

--- dev/postgres_init/add_readonly_user.sql
+++ dev/postgres_init/add_readonly_user.sql
@@ -1,6 +1,7 @@
-CREATE USER reader_user WITH PASSWORD 'reader_user';
+CREATE USER reader_user
+WITH PASSWORD 'reader_user';
 
 GRANT CONNECT ON DATABASE postgres TO reader_user;
--- This assumes you're actually connected to mydb..
+-- This assumes you're actually connected to the postgres database..
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
-   GRANT SELECT ON TABLES TO reader_user;
+GRANT SELECT ON TABLES TO reader_user;
