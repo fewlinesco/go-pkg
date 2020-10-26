b45b10ce75981e0816ac968e8258f01301f318c9
Kemosabert <bert.coppens14@gmail.com>
2020-10-26 14:46:10 +0100

    rename the structs

--- platform/web/api_server.go
+++ platform/web/api_server.go
@@ -11,22 +11,22 @@ import (
 	"github.com/fewlinesco/go-pkg/platform/metrics"
 )
 
-type ApiServer struct {
+type APIServer struct {
 	Name   string
 	Config ServerConfig
 	Server *http.Server
 }
 
-type apiServerError struct {
+type APIServerError struct {
 	Origin string
 	Error  error
 }
 
-func (s *ApiServer) Start(errorChannel chan apiServerError) {
+func (s *APIServer) Start(errorChannel chan APIServerError) {
 	go func() {
 		err := s.Server.ListenAndServe()
 
-		errorChannel <- apiServerError{
+		errorChannel <- APIServerError{
 			Origin: s.Name,
 			Error:  err,
 		}
@@ -47,15 +47,15 @@ func CreateMetricsHandler(namespace string) (http.Handler, error) {
 	return handler, nil
 }
 
-func CreateMonitoringAPIServer(serverConfig ServerConfig, logger *logging.Logger, metricsHandler http.Handler, serviceCheckers []HealthzChecker) ApiServer {
-	return ApiServer{
+func CreateMonitoringAPIServer(serverConfig ServerConfig, logger *logging.Logger, metricsHandler http.Handler, serviceCheckers []HealthzChecker) APIServer {
+	return APIServer{
 		Name:   "monitoring",
 		Config: serverConfig,
 		Server: NewMonitoringServer(serverConfig, logger, WrapNetHTTPHandler("metrics", metricsHandler), serviceCheckers),
 	}
 }
 
-func MonitorAPIServerShutdown(logger *logging.Logger, servers []ApiServer, serverErr chan apiServerError, shutdown chan os.Signal) error {
+func MonitorAPIServerShutdown(logger *logging.Logger, servers []APIServer, serverErr chan APIServerError, shutdown chan os.Signal) error {
 	select {
 	case apiErr := <-serverErr:
 		logger.Printf("the '%s' server exited with an error: %v", apiErr.Origin, apiErr.Error)
@@ -79,7 +79,7 @@ func MonitorAPIServerShutdown(logger *logging.Logger, servers []ApiServer, serve
 	return nil
 }
 
-func HandleAPIServerShutdown(server ApiServer, logger *logging.Logger) error {
+func HandleAPIServerShutdown(server APIServer, logger *logging.Logger) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(server.Config.ShutdownTimeout)*time.Second)
 	defer cancel()
 
