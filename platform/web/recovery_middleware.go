e22adc376c1926c62a4503db54576857c586a5bb
julien-leclercq <julien@fewlines.co>
2020-12-01 09:52:34 +0100

    Cu awwdr5 fix recovery middleware in go pkg (#42)
    This pull request make recovery middleware log and recover panics properly
    
    I also added traceid as a detail in the unmanaged error so that we could have better debugging experience

--- platform/web/recovery_middleware.go
+++ platform/web/recovery_middleware.go
@@ -6,24 +6,39 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/fewlinesco/go-pkg/platform/logging"
 	"github.com/getsentry/sentry-go"
 	"go.opencensus.io/trace"
 )
 
 // RecoveryMiddleware recovers panic errors to send a classical 500. It also sends the error to Sentry
-func RecoveryMiddleware() Middleware {
+func RecoveryMiddleware(logger *logging.Logger) Middleware {
 	return func(before Handler) Handler {
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request, params map[string]string) (err error) {
 			ctx, span := trace.StartSpan(ctx, "internal.web.RecoveryMiddleware")
+			defer span.End()
+
+			v := ctx.Value(KeyValues).(*Values)
+			elapsedTime := time.Since(v.Now)
 
 			defer func() {
-				if err := recover(); err != nil {
+
+				if recoverErr := recover(); recoverErr != nil {
+
 					v := ctx.Value(KeyValues).(*Values)
 
 					sentry.CurrentHub().Recover(err)
 					sentry.Flush(2 * time.Second)
 
-					err = fmt.Errorf("a panic has been recovered: %w: %v ", NewErrUnmanagedResponse(v.TraceID), err)
+					logger.PrintRequestResponse(
+						logging.RequestAttribute(r.Method, r.URL.Path, http.StatusInternalServerError),
+						logging.TraceAttribute(v.TraceID),
+						logging.DurationAttribute(elapsedTime),
+						logging.RemoteAddressAttribute(r.RemoteAddr),
+						fmt.Errorf("a panic has been recovered: %v", recoverErr).Error(),
+					)
+					_ = Respond(ctx, w, NewErrUnmanagedResponse(v.TraceID), http.StatusInternalServerError)
+					err = nil
 				}
 				span.End()
 			}()
