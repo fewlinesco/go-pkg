54670a6b8e3a4785048abe0959c5b03a01d1d93d
Julien Leclercq <julien.leclercq@gmx.fr>
2020-12-04 12:07:17 +0100

    clean tests

--- platform/database/tests/database_test.go
+++ platform/database/tests/database_test.go
@@ -3,12 +3,24 @@ package tests
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"net/url"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/GuiaBolso/darwin"
 	"github.com/fewlinesco/go-pkg/platform/database"
+	"github.com/jmoiron/sqlx"
 )
+
+type testData struct {
+	ID    string `database:"id"`
+	Value string `database:"value"`
+}
+
+var firstData = testData{ID: "ef79f1d4-4150-45ff-b94d-9e4691cc05aa", Value: "first_value"}
+var secondData = testData{ID: "bdc90138-ee0f-456c-8e31-e92514fac45e", Value: "second_value"}
 
 func TestProdDatabase(t *testing.T) {
 	defer func() {
@@ -27,24 +39,414 @@ func TestProdDatabase(t *testing.T) {
 		t.Fatalf("can't parse file: %#v", err)
 	}
 
+	t.Run("ExecContext", func(t *testing.T) {
+		cleanup := migrate(cfg, t)
+		defer cleanup()
+		sqlxDB, err := connect(cfg)
+		if err != nil {
+			t.Fatalf("could not create sqlx connection: %#v", err)
+		}
+		defer sqlxDB.Close()
+
+		_, err = sqlxDB.NamedExecContext(
+			context.Background(),
+			`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
+			firstData,
+		)
+
+		if err != nil {
+			t.Fatalf("cannot setup test: %#v", err)
+		}
+
+		type testCase struct {
+			name           string
+			data           testData
+			shouldErr      bool
+			shouldFindData bool
+		}
+
+		tcs := []testCase{
+			{
+				name:           "when everything is fine",
+				data:           secondData,
+				shouldErr:      false,
+				shouldFindData: true,
+			},
+			{
+				name:           "when a constraint is not respected",
+				data:           firstData,
+				shouldErr:      true,
+				shouldFindData: false,
+			},
+		}
+
+		doTest := func(tc testCase, t *testing.T) {
+			t.Run(tc.name, func(t *testing.T) {
+				db, err := database.Connect(cfg)
+				if err != nil {
+					t.Fatalf("could not connect to the database: %#v", err)
+				}
+				defer db.Close()
+
+				_, err = db.ExecContext(
+					context.Background(),
+					`INSERT INTO test_data (id, value) VALUES ($1, $2);`,
+					tc.data.ID,
+					tc.data.Value,
+				)
+
+				if tc.shouldErr {
+					if err == nil {
+						t.Fatalf("erroring exec don't return an error (most likely panics)")
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("could not ExecContext %#v", err)
+					}
+				}
+
+				sqlxDB, err := connect(cfg)
+				defer sqlxDB.Close()
+
+				if tc.shouldFindData {
+					var getTestData testData
+					err = sqlxDB.GetContext(context.Background(), &getTestData, `SELECT * FROM test_data WHERE ID = $1`, tc.data.ID)
+					if err != nil {
+						t.Fatalf("could not get inserted data : %#v", err)
+					}
+
+					if getTestData.ID != tc.data.ID || getTestData.Value != tc.data.Value {
+						t.Fatalf("expected test data with ID : %s and Value: %s, but got %#v", tc.data.ID, tc.data.Value, getTestData)
+					}
+				}
+			})
+		}
+
+		for _, tc := range tcs {
+			doTest(tc, t)
+		}
+
+	})
+
+	t.Run("NamedExecContext", func(t *testing.T) {
+		cleanup := migrate(cfg, t)
+		defer cleanup()
+		sqlxDB, err := connect(cfg)
+		if err != nil {
+			t.Fatalf("could not create sqlx connection: %#v", err)
+		}
+		defer sqlxDB.Close()
+
+		_, err = sqlxDB.NamedExecContext(
+			context.Background(),
+			`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
+			testData{ID: firstData.ID, Value: firstData.Value},
+		)
+
+		if err != nil {
+			t.Fatalf("cannot setup test: %#v", err)
+		}
+
+		type testCase struct {
+			name           string
+			data           testData
+			shouldErr      bool
+			shouldFindData bool
+		}
+
+		tcs := []testCase{
+			{
+				name:           "when everything is fine",
+				data:           secondData,
+				shouldErr:      false,
+				shouldFindData: true,
+			},
+			{
+				name:           "when a constraint is not respected",
+				data:           firstData,
+				shouldErr:      true,
+				shouldFindData: false,
+			},
+		}
+
+		doTest := func(tc testCase, t *testing.T) {
+			t.Run(tc.name, func(t *testing.T) {
+				db, err := database.Connect(cfg)
+				if err != nil {
+					t.Fatalf("could not connect to the database: %#v", err)
+				}
+				defer db.Close()
+
+				_, err = db.NamedExecContext(
+					context.Background(),
+					`INSERT INTO test_data (id, value) VALUES (:id, :value);`,
+					tc.data,
+				)
+
+				if tc.shouldErr {
+					if err == nil {
+						t.Fatalf("erroring exec don't return an error (most likely panics)")
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("could not NamedExecContext %#v", err)
+					}
+				}
+
+				sqlxDB, err := connect(cfg)
+				defer sqlxDB.Close()
+
+				if tc.shouldFindData {
+					var getTestData testData
+					err = sqlxDB.GetContext(context.Background(), &getTestData, `SELECT * FROM test_data WHERE ID = $1`, tc.data.ID)
+					if err != nil {
+						t.Fatalf("could not get inserted data : %#v", err)
+					}
+
+					if getTestData.ID != tc.data.ID || getTestData.Value != tc.data.Value {
+						t.Fatalf("expected test data with ID : %s and Value: %s, but got %#v", tc.data.ID, tc.data.Value, getTestData)
+					}
+				}
+			})
+		}
+
+		for _, tc := range tcs {
+			doTest(tc, t)
+		}
+	})
+
+	t.Run("GetContext previously inserted data", func(t *testing.T) {
+		cleanup := migrate(cfg, t)
+		defer cleanup()
+		sqlxDB, err := connect(cfg)
+		if err != nil {
+			t.Fatalf("could not create sqlx connection: %#v", err)
+		}
+		defer sqlxDB.Close()
+
+		_, err = sqlxDB.NamedExecContext(
+			context.Background(),
+			`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
+			testData{ID: firstData.ID, Value: firstData.Value},
+		)
+
+		if err != nil {
+			t.Fatalf("cannot setup test: %#v", err)
+		}
+
+		type testCase struct {
+			name           string
+			data           testData
+			shouldErr      bool
+			shouldFindData bool
+		}
+
+		tcs := []testCase{
+			{
+				name:           "when data has been inserted before",
+				data:           firstData,
+				shouldErr:      false,
+				shouldFindData: true,
+			},
+			{
+				name:           "when a constraint is not respected",
+				data:           secondData,
+				shouldErr:      true,
+				shouldFindData: false,
+			},
+		}
+
+		doTest := func(tc testCase, t *testing.T) {
+			t.Run(tc.name, func(t *testing.T) {
+				db, err := database.Connect(cfg)
+				if err != nil {
+					t.Fatalf("could not connect to the database: %#v", err)
+				}
+				defer db.Close()
+
+				var getTestData testData
+				err = db.GetContext(context.Background(), &getTestData, `SELECT * FROM test_data WHERE ID = $1`, tc.data.ID)
+				if tc.shouldErr {
+					if err == nil {
+						t.Fatalf("erroring exec don't return an error (most likely panics)")
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("could not GetContext %#v", err)
+					}
+				}
+
+				if tc.shouldFindData {
+					if getTestData.ID != tc.data.ID || getTestData.Value != tc.data.Value {
+						t.Fatalf("expected test data with ID : %s and Value: %s, but got %#v", tc.data.ID, tc.data.Value, getTestData)
+					}
+				} else {
+					if !(reflect.Zero(reflect.TypeOf(getTestData)).Interface() == getTestData) {
+						t.Fatalf("getTestData should not have been populated but got %#v", err)
+					}
+				}
+			})
+		}
+
+		for _, tc := range tcs {
+			doTest(tc, t)
+		}
+	})
+
+	t.Run("SelectContext previously inserted data", func(t *testing.T) {
+		cleanup := migrate(cfg, t)
+		defer cleanup()
+		sqlxDB, err := connect(cfg)
+		if err != nil {
+			t.Fatalf("could not create sqlx connection: %#v", err)
+		}
+		defer sqlxDB.Close()
+
+		_, err = sqlxDB.NamedExecContext(
+			context.Background(),
+			`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
+			firstData,
+		)
+
+		if err != nil {
+			t.Fatalf("cannot setup test: %#v", err)
+		}
+
+		_, err = sqlxDB.NamedExecContext(
+			context.Background(),
+			`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
+			secondData,
+		)
+
+		if err != nil {
+			t.Fatalf("cannot setup test: %#v", err)
+		}
+
+		type testCase struct {
+			name      string
+			condition *struct {
+				sql string
+				arg string
+			}
+			shouldFindData []testData
+			shouldErr      bool
+		}
+
+		tcs := []testCase{
+			{
+				name:           "when no condition is provided, it gets all the data",
+				shouldFindData: []testData{firstData, secondData},
+				shouldErr:      false,
+			},
+			{
+				name: "when a condition is provided it only gets the requested data",
+				condition: &struct {
+					sql string
+					arg string
+				}{sql: "WHERE id = $1", arg: firstData.ID},
+				shouldFindData: []testData{firstData},
+				shouldErr:      false,
+			},
+			{
+				name: "when no data is matching the condition, it returns an empty slice with no error",
+				condition: &struct {
+					sql string
+					arg string
+				}{sql: "WHERE id = $1", arg: "3237b466-b3c6-4521-96c5-61022c4a1796"},
+				shouldFindData: []testData{},
+				shouldErr:      false,
+			},
+			{
+				name: "when the condition is faulty, it does not populate the slice and return an error",
+				condition: &struct {
+					sql string
+					arg string
+				}{sql: "WHERE non_exisiting_field = ", arg: firstData.ID},
+				shouldFindData: []testData{},
+				shouldErr:      true,
+			},
+		}
+
+		doTest := func(tc testCase, t *testing.T) {
+			t.Run(tc.name, func(t *testing.T) {
+				db, err := database.Connect(cfg)
+				if err != nil {
+					t.Fatalf("could not connect to the database: %#v", err)
+				}
+				defer db.Close()
+
+				var selectTestData []testData
+				if tc.condition == nil {
+					err = db.SelectContext(context.Background(), &selectTestData, `SELECT * FROM test_data;`)
+				} else {
+					err = db.SelectContext(context.Background(), &selectTestData, fmt.Sprintf("%s %s;", `SELECT * FROM test_data`, tc.condition.sql), tc.condition.arg)
+				}
+
+				if tc.shouldErr {
+					if err == nil {
+						t.Fatalf("erroring exec don't return an error (most likely panics)")
+					}
+				} else {
+					if err != nil {
+						t.Fatalf("could not GetContext %#v", err)
+					}
+				}
+
+				for _, sfd := range tc.shouldFindData {
+					found := false
+					for _, std := range selectTestData {
+						if std.ID == sfd.ID {
+							found = true
+						}
+					}
+					if !found {
+						t.Fatalf("should find %#v in selectTestData but got %#v", sfd, selectTestData)
+					}
+				}
+
+				for _, std := range selectTestData {
+					found := false
+					for _, sfd := range tc.shouldFindData {
+						if std.ID == sfd.ID {
+							found = true
+						}
+					}
+					if !found {
+						t.Fatalf("found %#v in selectTestData which is not in %#v", std, tc.shouldFindData)
+					}
+				}
+			})
+		}
+
+		for _, tc := range tcs {
+			doTest(tc, t)
+		}
+	})
+}
+
+func migrate(cfg database.Config, t *testing.T) func() {
 	db, err := database.Connect(cfg)
 	if err != nil {
 		t.Fatalf("could not connect to DB: %#v, with config: %#v", err, cfg)
 	}
-	defer func() { db.Close() }()
+	defer db.Close()
 
 	err = database.Migrate(db, []darwin.Migration{
 		{
 			Version:     1,
 			Description: "Create test data table",
 			Script: `CREATE TABLE test_data(
-				id UUID PRIMARY KEY,
-				value VARCHAR(63)
-			)`,
+					id UUID PRIMARY KEY,
+					value VARCHAR(63)
+					)`,
 		},
 	})
 
-	defer func() {
+	if err != nil {
+		t.Fatalf("could not migrate the database: %#v", err)
+	}
+
+	return func() {
 		db, err := database.Connect(cfg)
 		if err != nil {
 			t.Fatalf("could not connect to DB: %#v", err)
@@ -55,226 +457,24 @@ func TestProdDatabase(t *testing.T) {
 		if err != nil {
 			t.Fatalf("could not clean the database: %#v", err)
 		}
-	}()
-
-	if err != nil {
-		t.Fatalf("could not migrate the database: %#v", err)
 	}
 
-	// recreating a database each time to prove it can persist across different connections
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to the database: %#v", err)
-	}
-
-	firstUUID := "ef79f1d4-4150-45ff-b94d-9e4691cc05aa"
-	firstValue := "first_value"
-
-	_, err = db.ExecContext(
-		context.Background(),
-		`INSERT INTO test_data (id, value) VALUES ($1, $2);`,
-		firstUUID,
-		firstValue,
-	)
-
-	if err != nil {
-		t.Fatalf("could not ExecContext %#v", err)
-	}
-
-	_, err = db.ExecContext(
-		context.Background(),
-		`INSERT INTO test_data (id, value) VALUES ($1, $2);`,
-		firstUUID,
-		firstValue,
-	)
-
-	if err == nil {
-		t.Fatalf("erroring exec don't return an error (most likely panics)")
-	}
-
-	type testData struct {
-		ID    string `database:"id"`
-		Value string `database:"value"`
-	}
-
-	secondUUID := "bdc90138-ee0f-456c-8e31-e92514fac45e"
-	secondValue := "second_value"
-
-	_, err = db.NamedExecContext(
-		context.Background(),
-		`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
-		testData{ID: secondUUID, Value: secondValue},
-	)
-
-	if err != nil {
-		t.Fatalf("could not NamedExecContext: %#v", err)
-	}
-	db.Close()
-
-	// recreating a database each time to prove it can persist across different connections
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to the database: %#v", err)
-	}
-
-	var getTestData testData
-	err = db.GetContext(
-		context.Background(),
-		&getTestData,
-		`SELECT * from test_data WHERE ID = $1`,
-		firstUUID,
-	)
-
-	if err != nil {
-		t.Fatalf("could not GetContext first test data: %#v", err)
-	}
-
-	if getTestData.ID != firstUUID || getTestData.Value != firstValue {
-		t.Fatalf("expected test data with ID : %s and Value: %s, but got %#v", firstUUID, firstValue, getTestData)
-	}
-
-	db.Close()
-
-	// recreating a database each time to prove it can persist across different connections
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to the database: %#v", err)
-	}
-
-	var selectTestData []testData
-	err = db.SelectContext(
-		context.Background(),
-		&selectTestData,
-		"SELECT * from test_data",
-	)
-
-	if err != nil {
-		t.Fatalf("could not SelectContext: %#v", err)
-	}
-
-	checkTestData := map[string]bool{
-		"first_row":  false,
-		"second_row": false,
-	}
-
-	for _, row := range selectTestData {
-		if row.ID == firstUUID && row.Value == firstValue && checkTestData["first_row"] == false {
-			checkTestData["first_row"] = true
-		} else if row.ID == secondUUID && row.Value == secondValue && checkTestData["second_row"] == false {
-			checkTestData["second_row"] = true
-		} else {
-			t.Fatalf("Expected selectTestData to be first test data and second test data but got %#v", selectTestData)
+}
+func connect(config database.Config) (*sqlx.DB, error) {
+	if config.URL == "" {
+		options := make(url.Values)
+		for key, value := range config.Options {
+			options.Add(key, value)
 		}
+
+		connectionURL := url.URL{
+			Scheme:   config.Scheme,
+			User:     url.UserPassword(config.Username, config.Password),
+			Host:     fmt.Sprintf("%s:%d", config.Host, config.Port),
+			Path:     config.Database,
+			RawQuery: options.Encode(),
+		}
+		config.URL = connectionURL.String()
 	}
-
-	if !checkTestData["first_row"] {
-		t.Fatalf("expect selectTestData to carry firstValue but got %#v", selectTestData)
-	}
-
-	if !checkTestData["second_row"] {
-		t.Fatalf("expect selectTestData to carry secondValue but got %#v", selectTestData)
-	}
-
-	db.Close()
-
-	// recreating a database each time to prove it can persist across different connections
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to the database: %#v", err)
-	}
-
-	tx, err := db.Begin()
-
-	if err != nil {
-		t.Fatalf("could not start a transaction: %#v", err)
-	}
-	defer func() { tx.Rollback() }()
-
-	_, err = tx.ExecContext(context.Background(), "UPDATE test_data SET value = $1 WHERE id = $2", "updated_value", firstUUID)
-	if err != nil {
-		t.Fatalf("could not ExecContext in a transaction: %#v", err)
-	}
-
-	// This is inserting a primary key already existing in order to corrupt the transaction
-	_, err = tx.ExecContext(
-		context.Background(),
-		`INSERT INTO test_data (id, value) VALUES ($1, $2);`,
-		firstUUID,
-		firstValue,
-	)
-
-	if err == nil {
-		t.Fatalf("ExecContext should return an error")
-	}
-
-	err = tx.Commit()
-	if err == nil {
-		t.Fatalf("should not be able to commit faulty transaction")
-	}
-
-	err = db.Close()
-	if err != nil {
-		t.Fatalf("COULD NOT CLOSE THE DATABASE AAAAAAAAAAAH: %v", err)
-	}
-
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to database: %v", err)
-	}
-
-	tx, err = db.Begin()
-
-	err = db.GetContext(
-		context.Background(),
-		&getTestData,
-		`SELECT * from test_data WHERE ID = $1`,
-		firstUUID,
-	)
-	if err != nil {
-		t.Fatalf("could not use getContext %#v", err)
-	}
-
-	if getTestData.Value != firstValue {
-		t.Fatalf("A part of the rollbacked transaction has been committed")
-	}
-
-	if err != nil {
-		t.Fatalf("could not start a transaction: %#v", err)
-	}
-
-	thirdUUID := "5afb4fef-4ecb-4ca3-81fa-10306a9cbb60"
-	thirdValue := "third_value"
-	_, err = db.NamedExecContext(
-		context.Background(),
-		`INSERT INTO test_data (id, value) VALUES (:id, :value)`,
-		testData{ID: thirdUUID, Value: thirdValue},
-	)
-
-	if err != nil {
-		t.Fatalf("could not shoot named exec context to the database: %#v", err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		t.Fatalf("could not commit transaction")
-	}
-
-	db.Close()
-
-	// recreating a database each time to prove it can persist across different connections
-	db, err = database.Connect(cfg)
-	if err != nil {
-		t.Fatalf("could not connect to the database: %#v", err)
-	}
-
-	err = db.GetContext(
-		context.Background(),
-		&getTestData,
-		`SELECT * from test_data WHERE ID = $1`,
-		thirdUUID,
-	)
-	if err != nil {
-		t.Fatalf("could not get data inserted in a transaction %#v", err)
-	}
-
+	return sqlx.Connect(config.Driver, config.URL)
 }
