60d0bf808f2ae39cbcf2e88c459a001393424e0d
Kemosabert <bert.coppens14@gmail.com>
2021-01-27 17:40:50 +0100

    :recycle: update the tests

--- platform/web/retry/tests/middleware_test.go
+++ platform/web/retry/tests/middleware_test.go
@@ -21,7 +21,7 @@ func TestRetryRoundTripperMiddleware(t *testing.T) {
 
 	cfg := retry.Config{
 		MaxRetry: 5,
-		ExceptOn: []int{http.StatusUnauthorized},
+		ExceptOn: []int{http.StatusOK, http.StatusNotFound},
 	}
 
 	transport := http.DefaultTransport.(*http.Transport).Clone()
@@ -37,33 +37,64 @@ func TestRetryRoundTripperMiddleware(t *testing.T) {
 			expectedCalls:     1,
 		},
 		{
-			name:              "it_does_all_retries_when_the_api_returns_error",
-			expectedHTTPCode: 	http.StatusBadRequest,
-			httpCodesToReturn: []int{http.StatusBadRequest, http.StatusBadRequest, http.StatusBadRequest, http.StatusBadRequest, http.StatusBadRequest, http.StatusBadRequest},
+			name:              "it_returns_the_last_response_when_no_request_is_successful",
+			expectedHTTPCode: 	http.StatusForbidden,
+			httpCodesToReturn: []int{
+				http.StatusUnprocessableEntity,
+				http.StatusUnprocessableEntity,
+				http.StatusUnprocessableEntity,
+				http.StatusUnprocessableEntity,
+				http.StatusUnprocessableEntity,
+				http.StatusForbidden,
+			},
 			expectedCalls:     6,
 		},
 		{
-			name:              "it_stops_retrying_when_api_returns_success",
-			expectedHTTPCode: 	http.StatusNoContent,
-			httpCodesToReturn: []int{http.StatusBadRequest, http.StatusBadRequest, http.StatusNoContent},
+			name:              "it_stops_retrying_when_api_returns_expected_code",
+			expectedHTTPCode: 	http.StatusOK,
+			httpCodesToReturn: []int{http.StatusBadRequest, http.StatusBadRequest, http.StatusOK},
 			expectedCalls:     3,
+		},
+		{
+			name:              "it_stops_retrying_when_api_returns_other_expected_code",
+			expectedHTTPCode: 	http.StatusNotFound,
+			httpCodesToReturn: []int{
+				http.StatusForbidden,
+				http.StatusForbidden,
+				http.StatusForbidden,
+				http.StatusForbidden,
+				http.StatusNotFound,
+			},
+			expectedCalls:     5,
 		},
 	}
 
 	for _, tc := range tcs {
 		t.Run(tc.name, func(t *testing.T) {
+		tc := tc
 			var requestTimestamps []time.Time
 
 			handler := func() http.HandlerFunc {
 				var lock sync.Mutex
-				var count int
+				var count = 0
 
 				return func(w http.ResponseWriter, r *http.Request) {
+					defer func() {
+						if err := recover(); err != nil {
+							t.Fatalf("PANIC RECOVERED: %+v", err)
+						}
+					}()
 					lock.Lock()
+					defer lock.Unlock()
+
+					if len(tc.httpCodesToReturn) <= count {
+						t.Fatalf("the handler was called more than foreseen. Current count: %d, expectedCount: %d", count, len(tc.httpCodesToReturn))
+					}
+
 					requestTimestamps = append(requestTimestamps, time.Now())
 					w.WriteHeader(tc.httpCodesToReturn[count])
 					count++
-					lock.Unlock()
+
 				}
 			}
 
@@ -74,7 +105,6 @@ func TestRetryRoundTripperMiddleware(t *testing.T) {
 			if err != nil {
 				t.Fatalf("an error occured while dispatching the request: %v", err)
 			}
-
 
 			if len(requestTimestamps) != tc.expectedCalls {
 				t.Fatalf("Expected the handler to be counted: %d times, but it was called: %d time(s)", tc.expectedCalls, len(requestTimestamps))
