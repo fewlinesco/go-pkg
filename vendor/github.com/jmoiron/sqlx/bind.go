07a377f56bf9337d810f3abf3aff22a5ed6bd2d3
Kemosabert <bert.coppens14@gmail.com>
2021-01-28 15:27:49 +0100

    :recycle: update some vendored dependencies

--- vendor/github.com/jmoiron/sqlx/bind.go
+++ vendor/github.com/jmoiron/sqlx/bind.go
@@ -22,10 +22,10 @@ const (
 )
 
 var defaultBinds = map[int][]string{
-	DOLLAR:   {"postgres", "pgx", "pq-timeouts", "cloudsqlpostgres", "ql"},
-	QUESTION: {"mysql", "sqlite3"},
-	NAMED:    {"oci8", "ora", "goracle"},
-	AT:       {"sqlserver"},
+	DOLLAR:   []string{"postgres", "pgx", "pq-timeouts", "cloudsqlpostgres", "ql"},
+	QUESTION: []string{"mysql", "sqlite3"},
+	NAMED:    []string{"oci8", "ora", "goracle"},
+	AT:       []string{"sqlserver"},
 }
 
 var binds sync.Map
