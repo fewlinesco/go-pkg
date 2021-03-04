4f82c420ad5451f075c1ca8f1b52cbe42e71229d
Florian Thelliez <florian.thelliez@gmail.com>
2021-03-04 17:50:16 +0100

    Add tests and move the test json schema as it cannot be in a higher directory to embed it in the tests

--- platform/web/tests/decode_with_embedded_json_schema_test.go
+++ platform/web/tests/decode_with_embedded_json_schema_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	_ "embed"
 	"errors"
 	"net/http"
 	"reflect"
@@ -10,30 +11,26 @@ import (
 	"github.com/fewlinesco/go-pkg/platform/web"
 )
 
-type decodeWithJSONSchemaTestData struct {
+type decodeWithEmbeddedJSONSchemaTestData struct {
 	Name             string
 	Body             string
 	ValidateResponse func(testProvider *testing.T, outcome error, data decodeWithJSONSchemaTestData)
-	JSONSchemaPath   string
+	JSONSchema       []byte
 	DecoderOptions   web.DecoderOptions
 	ExpectedError    error
 	ExpectedOutcome  expectedModel
 }
 
-type expectedModel struct {
-	ID       string            `json:"id"`
-	Code     string            `json:"code"`
-	DataType string            `json:"datatype"`
-	Name     map[string]string `json:"name"`
-}
+//go:embed testdata/json_schema_with_definition.json
+var jsonSchema []byte
 
-func TestDecodeWithJSONSchema(t *testing.T) {
+func TestDecodeWithEmbeddedJSONSchema(t *testing.T) {
 
-	tcs := []decodeWithJSONSchemaTestData{
+	tcs := []decodeWithEmbeddedJSONSchemaTestData{
 		{
 			Name:           "when the decoding happens without an error",
 			Body:           `{"code": "code", "id": "d43c45b0-f420-4de9-8745-6e3840ab39fd", "datatype": "integer"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError:  nil,
 			ExpectedOutcome: expectedModel{
@@ -43,16 +40,9 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 			},
 		},
 		{
-			Name:           "when the path to the json schema is incorrect",
-			Body:           `{"code": "code", "id": "c9ecb26a-20ab-4acb-b34e-444457b06b3b", "datatype": "string"}`,
-			JSONSchemaPath: "testdata/json_schema/json_schema_with_definition.json",
-			DecoderOptions: web.DecoderOptions{},
-			ExpectedError:  web.NewErrBadRequestResponse(nil),
-		},
-		{
 			Name:           "when a parameter has an incorrect datatype",
 			Body:           `{"code": 1, "id": "815b73a1-3d89-4c68-a4d8-1f36c091a533", "datatype": "string"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrInvalidRequestBodyContent(web.ErrorDetails{
 				"code": "Invalid type. Expected: string, given: integer",
@@ -61,7 +51,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "when a parameter has an incorrect enum type",
 			Body:           `{"code": "code", "id": "10fbd107-4bcf-4c91-8ee2-957e07d6109e", "datatype": "hello"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrInvalidRequestBodyContent(web.ErrorDetails{
 				"datatype": `datatype must be one of the following: "string", "boolean", "localizedString", "integer", "number"`,
@@ -70,7 +60,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "when the json has an unknown field and the decoder options are empty",
 			Body:           `{"code": "code", "id": "78c8803e-ce4e-474e-97c4-7bd6d565ddca", "datatype": "string", "unknown_field": "hello"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrBadRequestResponse(web.ErrorDetails{
 				"unknown_field": "unknown_field field is not allowed",
@@ -79,7 +69,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "when the json has an unknown field and the decoder options specify it should not allow unknown fields ",
 			Body:           `{"code": "code", "id": "ec85bd34-67bf-4418-95cb-2616e914bfc9", "datatype": "string", "unknown_field": "hello"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{AllowUnknownFields: false},
 			ExpectedError: web.NewErrBadRequestResponse(web.ErrorDetails{
 				"unknown_field": "unknown_field field is not allowed",
@@ -88,7 +78,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "when the json has an unknown field and the decoder options specify it should not allow unknown fields ",
 			Body:           `{"code": "code", "id": "8321308a-4cae-4175-8c56-2db087e5ca10", "datatype": "integer", "unknown_field": "hello"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{AllowUnknownFields: true},
 			ExpectedError:  nil,
 			ExpectedOutcome: expectedModel{
@@ -100,7 +90,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "it returns a bad request reponse when a required key is missing",
 			Body:           `{"code": "code", "datatype": "integer"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrBadRequestResponse(web.ErrorDetails{
 				"id": "id is required",
@@ -109,7 +99,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "it returns a bad request reponse when a required key is missing and there is an issue with another property",
 			Body:           `{"code": 5, "datatype": "integer"}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrBadRequestResponse(web.ErrorDetails{
 				"id":   "id is required",
@@ -119,7 +109,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "It can properly validate the localized string",
 			Body:           `{"code": "code", "id": "d43c45b0-f420-4de9-8745-6e3840ab39fd", "datatype": "integer", "name": {"en-US": "this is a test", "fr-FR": "another test"}}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError:  nil,
 			ExpectedOutcome: expectedModel{
@@ -132,7 +122,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "It throws an error when a required nested property is missing",
 			Body:           `{"code": "code", "id": "d43c45b0-f420-4de9-8745-6e3840ab39fd", "datatype": "integer", "name": {"fr-FR": "another test"}}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrBadRequestResponse(web.ErrorDetails{
 				"name": "en-US is required",
@@ -141,7 +131,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 		{
 			Name:           "It throws an error when a required nested property is missing",
 			Body:           `{"code": "code", "id": "d43c45b0-f420-4de9-8745-6e3840ab39fd", "datatype": "integer", "name": {"en-US": "this is a test", "French": "ceci est une test"}}`,
-			JSONSchemaPath: "testdata/json_schema_with_definition.json",
+			JSONSchema:     jsonSchema,
 			DecoderOptions: web.DecoderOptions{},
 			ExpectedError: web.NewErrInvalidRequestBodyContent(web.ErrorDetails{
 				"name": "Additional property French is not allowed",
@@ -161,7 +151,7 @@ func TestDecodeWithJSONSchema(t *testing.T) {
 			request.Header.Set("Content-Type", "application/json")
 
 			var result expectedModel
-			if err := web.DecodeWithJSONSchema(request, &result, tc.JSONSchemaPath, tc.DecoderOptions); err != nil {
+			if err := web.DecodeWithEmbeddedJSONSchema(request, &result, tc.JSONSchema, tc.DecoderOptions); err != nil {
 				if tc.ExpectedError == nil {
 					t.Fatalf("the request body failed the validation but the test did not expect this: %v", err)
 				}
