87509b40f2e89afe307b7e61d8c3f61d263de70a
Florian Thelliez <florian.thelliez@gmail.com>
2021-08-04 09:20:34 +0200

    fix error

--- platform/jsonschema/jsonschema.go
+++ platform/jsonschema/jsonschema.go
@@ -28,6 +28,10 @@ type ValidationError struct {
 	Details map[string]string
 }
 
+func (err ValidationError) Error() string {
+	return "the json input is not valid against the JSON schema"
+}
+
 // ValidateJSONAgainstSchema validate some json data against a json schema and return detailed validation errors if any is present.
 func ValidateJSONAgainstSchema(jsonData []byte, jsonSchema []byte) error {
 	dataLoader := gojsonschema.NewBytesLoader(jsonData)
