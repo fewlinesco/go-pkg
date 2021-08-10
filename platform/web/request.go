cf786db7777d5a160d7ea03effe1deeee543c7ea
Florian Thelliez <florian.thelliez@gmail.com>
2021-08-10 23:26:17 +0200

    Refactor the existing functions to use the new validate JSON schema function

--- platform/web/request.go
+++ platform/web/request.go
@@ -3,17 +3,19 @@ package web
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/fewlinesco/go-pkg/platform/jsonschema"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 	"reflect"
 	"regexp"
 	"runtime"
 	"strings"
 
-	"github.com/fewlinesco/gojsonschema"
 	en "github.com/go-playground/locales/en"
 	ut "github.com/go-playground/universal-translator"
 	validator "gopkg.in/go-playground/validator.v9"
@@ -70,7 +72,6 @@ func Decode(r *http.Request, val interface{}) error {
 
 // DecodeWithJSONSchema takes the path to a json schema and a http request
 // And returns an error when the request's payload does not match the JSON schema
-// Deprecated: Use the jsonschema package ValidateJson to validate the json
 func DecodeWithJSONSchema(request *http.Request, model interface{}, filePath string, options DecoderOptions) error {
 	_, rootFile, _, ok := runtime.Caller(1)
 	if !ok {
@@ -79,9 +80,12 @@ func DecodeWithJSONSchema(request *http.Request, model interface{}, filePath str
 
 	filePath = path.Join(path.Dir(rootFile), filePath)
 
-	jsonSchema := gojsonschema.NewReferenceLoader("file://" + filePath)
+	jsonSchema, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("%w", NewErrInvalidJSONSchemaFilePath())
+	}
 
-	if err := validateRequestPayload(request, model, options, jsonSchema); err != nil {
+	if err := DecodeWithEmbeddedJSONSchema(request, model, jsonSchema, options); err != nil {
 		return err
 	}
 	return nil
@@ -89,17 +93,7 @@ func DecodeWithJSONSchema(request *http.Request, model interface{}, filePath str
 
 // DecodeWithEmbeddedJSONSchema takes json schema and a http request
 // And returns an error when the request's payload does not match the JSON schema
-// Deprecated: Use the jsonschema package ValidateJson to validate the json
-func DecodeWithEmbeddedJSONSchema(request *http.Request, model interface{}, jsonSchemaBytes []byte, options DecoderOptions) error {
-	jsonSchema := gojsonschema.NewBytesLoader(jsonSchemaBytes)
-
-	if err := validateRequestPayload(request, model, options, jsonSchema); err != nil {
-		return err
-	}
-	return nil
-}
-
-func validateRequestPayload(request *http.Request, model interface{}, options DecoderOptions, jsonSchema gojsonschema.JSONLoader) error {
+func DecodeWithEmbeddedJSONSchema(request *http.Request, model interface{}, jsonSchema []byte, options DecoderOptions) error {
 	body, err := io.ReadAll(request.Body)
 	if err != nil {
 		if err.Error() == "http: request body too large" {
@@ -108,56 +102,16 @@ func validateRequestPayload(request *http.Request, model interface{}, options De
 		return fmt.Errorf("could not read request body: %v", err)
 	}
 	request.Body = io.NopCloser(bytes.NewBuffer(body))
-	payload := gojsonschema.NewBytesLoader(body)
 
-	result, err := gojsonschema.Validate(jsonSchema, payload)
-	if err != nil {
-		return fmt.Errorf("%w: %v", NewErrBadRequestResponse(nil), err)
-	}
-
-	if !result.Valid() {
-		additionalPropertyErrors := make(ErrorDetails)
-		requiredPropertyErrors := make(ErrorDetails)
-		requestContentErrorDetails := make(ErrorDetails)
-
-		for _, desc := range result.Errors() {
-			propertyName := desc.Field()
-			if propertyName == "(root)" {
-				details, ok := desc.Details()["property"]
-				if ok {
-					propertyName = fmt.Sprintf("%v", details)
-				}
-			}
-
-			switch desc.Type() {
-			case "additional_property_not_allowed":
-				additionalPropertyErrors[propertyName] = desc.Description()
-			case "required":
-				requiredPropertyErrors[propertyName] = desc.Description()
-			default:
-				requestContentErrorDetails[propertyName] = desc.Description()
-			}
+	if err := jsonschema.ValidateJSON(body, jsonSchema); err != nil {
+		switch unwrappedErr := errors.Unwrap(err).(type) {
+		case jsonschema.AdditionalPropertyError:
+			return fmt.Errorf("jsonschema validation error: %w", NewErrBadRequestResponse(unwrappedErr.Details))
+		case jsonschema.MissingPropertyError:
+			return fmt.Errorf("jsonschema validation error: %w", NewErrBadRequestResponse(unwrappedErr.Details))
+		case jsonschema.ValidationError:
+			return fmt.Errorf("jsonschema validation error: %w", NewErrInvalidRequestBodyContent(unwrappedErr.Details))
 		}
-
-		if len(additionalPropertyErrors) > 0 {
-			errDetails := additionalPropertyErrors
-			for property, errMessage := range requestContentErrorDetails {
-				errDetails[property] = errMessage
-			}
-
-			return fmt.Errorf("the request body contains unknown keys: %w", NewErrBadRequestResponse(errDetails))
-		}
-
-		if len(requiredPropertyErrors) > 0 {
-			errDetails := requiredPropertyErrors
-			for property, errMessage := range requestContentErrorDetails {
-				errDetails[property] = errMessage
-			}
-
-			return fmt.Errorf("the request body contains unknown keys: %w", NewErrBadRequestResponse(errDetails))
-		}
-
-		return fmt.Errorf("%w", NewErrInvalidRequestBodyContent(requestContentErrorDetails))
 	}
 
 	if err := decode(request, model, options); err != nil {
